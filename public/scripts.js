0408fe7cfe8ffa3b2dd7f726911b8686cc2de2be
Chris Lamberth <cmattic@gmail.com>
2023-09-07 14:30:30 -0400

    Added Coupon functionality

--- public/scripts.js
+++ public/scripts.js
@@ -2,18 +2,17 @@
 function normalizeBrandText(content) {
 	const regex = /\[\s*brand\s*\]/gi; // matches variations like [brand], [ brand ], [Brand], [Brand ], [ brand], [ Brand], etc.
 	return content.replace(regex, '[Brand]');
- }
- 
- // Listen for input events
- document.addEventListener('input', function(event) {
+}
+
+// Listen for input events
+document.addEventListener('input', function(event) {
 	const target = event.target;
- 
+
 	// Check if the target is an input or textarea
-	if (target.tagName.toLowerCase() === 'input' || target.tagName.toLowerCase() === 'textarea') {
-	  const value = target.value;
-	  
-	  // Normalize the brand text
-	  target.value = normalizeBrandText(value);
+	if ((target.tagName.toLowerCase() === 'input' && target.type !== 'file') || target.tagName.toLowerCase() === 'textarea') {
+		 const value = target.value;
+		 
+		 // Normalize the brand text
+		 target.value = normalizeBrandText(value);
 	}
- });
- 
+});
--- server.js
+++ server.js
@@ -10,24 +10,29 @@ const archiver = require('archiver');
 const app = express();
 const port = 3000;
 
-// Function to replace placeholders
 function replacePlaceholders(input, brandName) {
     return input.replace(/\[Brand\]/g, brandName);
 }
 
-// Setting up EJS
-app.set('view engine', 'ejs');
+function getBrandObjectNameFromName(jsonData, brandName) {
+    for (let category of ['service', 'tire']) {
+        for (let brandObject in jsonData[category]) {
+            if (jsonData[category][brandObject].name === brandName) {
+                return brandObject;
+            }
+        }
+    }
+    return null;
+}
 
-// Middleware for body parser
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Use public folder for static files
 app.use(express.static('public'));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, os.tmpdir());  // save to system temp directory
+    cb(null, os.tmpdir());
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
@@ -49,58 +54,69 @@ app.get('/', (req, res) => {
 	const serviceBrands = Object.values(jsonData.service).map(brand => brand.name);
 	const tireBrands = Object.values(jsonData.tire).map(brand => brand.name);
 	res.render('form_page', { serviceBrands, tireBrands });
- });
- 
+});
 
-app.post('/generate-emails', upload.fields([{ name: 'serviceHeaderImage', maxCount: 1 }, { name: 'tireHeaderImage', maxCount: 1 }]), async (req, res) => {
-  const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-  const archive = archiver('zip');
-  const tmpDirs = [];
+app.post('/generate-emails', upload.fields([{ name: 'serviceHeaderImage', maxCount: 1 }, { name: 'tireHeaderImage', maxCount: 1 }, { name: 'directory', maxCount: 100 }]), async (req, res) => {
+    const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    const archive = archiver('zip');
+    const tmpDirs = [];
 
-  for (let category of ['service', 'tire']) {
-    for (let brand in jsonData[category]) {
-      const brandData = jsonData[category][brand];
-      const tmpDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), `${category}-${brand}-`));
-      tmpDirs.push(tmpDir);
+    for (let category of ['service', 'tire']) {
+        for (let brand of Object.values(jsonData[category])) {
+            const brandObjectName = getBrandObjectNameFromName(jsonData, brand.name);
+            if (!brandObjectName) {
+                console.error(`No object name found for brand: ${brand.name}`);
+                continue;
+            }
 
-      const imagePath = path.join(tmpDir, 'images');
-      await fs.promises.mkdir(imagePath);
+            const brandData = jsonData[category][brandObjectName];
+            const tmpDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), `${category}-${brandObjectName}-`));
+            tmpDirs.push(tmpDir);
 
-      // Copy logo and header images to the images folder
-      await fs.promises.copyFile(brandData.logoURL, path.join(imagePath, 'logo.jpg'));
-      await fs.promises.copyFile(req.files[`${category}HeaderImage`][0].path, path.join(imagePath, 'header.jpg'));
+            const imagePath = path.join(tmpDir, 'images');
+            await fs.promises.mkdir(imagePath);
 
-      const emailContent = await ejs.renderFile('./views/email_template.ejs', {
-        title: replacePlaceholders(req.body[`${category}Title`], brandData.name),
-        bodyCopy: replacePlaceholders(req.body[`${category}BodyCopy`], brandData.name),
-        logoURL: path.join('images', 'logo.jpg'),
-        headerImage: path.join('images', 'header.jpg'),
-        colors: brandData.colors,
-        headerLinks: brandData.headerLinks,
-        footerLinks: brandData.footerLinks,
-        phone: brandData.phone,
-        name: brandData.name
-      });
+            await fs.promises.copyFile(brandData.logoURL, path.join(imagePath, 'logo.jpg'));
+            await fs.promises.copyFile(req.files[`${category}HeaderImage`][0].path, path.join(imagePath, 'header.jpg'));
 
-      fs.writeFileSync(path.join(tmpDir, 'index.html'), emailContent);
-      archive.directory(tmpDir, `${category}/${brand}`);
+            const couponNumbers = req.body[`${brand.name}Coupons`].split(',').map(num => num.trim());
+            const couponFiles = req.files['directory'].filter(file => couponNumbers.includes(file.originalname.split('_')[0]));
+            
+            for (let file of couponFiles) {
+                await fs.promises.copyFile(file.path, path.join(imagePath, file.originalname));
+            }
+
+            const couponPaths = couponFiles.map(file => path.join('images', file.originalname));
+
+            const emailContent = await ejs.renderFile('./views/email_template.ejs', {
+                title: replacePlaceholders(req.body[`${category}Title`], brandData.name),
+                bodyCopy: replacePlaceholders(req.body[`${category}BodyCopy`], brandData.name),
+                logoURL: path.join('images', 'logo.jpg'),
+                headerImage: path.join('images', 'header.jpg'),
+                colors: brandData.colors,
+                headerLinks: brandData.headerLinks,
+                footerLinks: brandData.footerLinks,
+                phone: brandData.phone,
+                name: brandData.name,
+                coupons: couponPaths
+            });
+
+            fs.writeFileSync(path.join(tmpDir, 'index.html'), emailContent);
+            archive.directory(tmpDir, `${category}/${brandObjectName}`);
+        }
     }
-  }
 
-  archive.finalize();
+    archive.finalize();
+    res.attachment('emails.zip');
+    archive.pipe(res);
 
-  // Send the ZIP file as a response
-  res.attachment('emails.zip');
-  archive.pipe(res);
-
-  // Once the ZIP is sent to the client, remove the temporary directories
-  res.on('finish', () => {
-    for (let dir of tmpDirs) {
-      fs.promises.rm(dir, { recursive: true });
-    }
-  });
+    res.on('finish', () => {
+        for (let dir of tmpDirs) {
+            fs.promises.rm(dir, { recursive: true });
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Email Generator is now running on http://localhost:${port}`);
+    console.log(`Email Generator is now running on http://localhost:${port}`);
 });
