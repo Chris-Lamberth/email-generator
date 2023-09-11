85651e065472d04dc9aeeaa466599c1c53baa449
Chris <cmattic@gmail.com>
2023-09-10 23:14:46 -0400

    Updats
    Added some data. Added conditions for custom notifications and a footer graphic. Updated footer links data structure so there can be columns without headers.

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const multer = require('multer');
 const ejs = require('ejs');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const os = require('os');
 const archiver = require('archiver'); 
 const sharp = require('sharp');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Utility functions
 function replacePlaceholders(input, brandName) {
     return input.replace(/\[Brand\]/g, brandName);
 }
 
-function getBrandObjectNameFromName(jsonData, brandName) {
+async function getBrandObjectNameFromName(jsonData, brandName) {
     for (let category of ['service', 'tire']) {
         for (let brandObject in jsonData[category]) {
             if (jsonData[category][brandObject].name === brandName) {
@@ -38,73 +38,78 @@ async function getImageDimensions(filePath) {
     };
 }
 
+// Middleware configuration
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, os.tmpdir());
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+    destination: os.tmpdir(),
+    filename: (req, file, cb) => {
+        const brandName = file.fieldname.split('Coupons')[0];
+        cb(null, `${brandName}-${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg') {
-    cb(null, true);
-  } else {
-    cb(new Error('Only .jpg files are allowed!'), false);
-  }
-};
+const upload = multer({ 
+    storage, 
+    fileFilter: (req, file, cb) => {
+        cb(null, file.mimetype === 'image/jpeg');
+    }
+});
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-app.get('/', (req, res) => {
-    const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+// Routes
+app.get('/', async (req, res) => {
+    const jsonData = JSON.parse(await fs.readFile('data.json', 'utf8'));
     const serviceBrands = Object.values(jsonData.service).map(brand => brand.name);
     const tireBrands = Object.values(jsonData.tire).map(brand => brand.name);
     res.render('form_page', { serviceBrands, tireBrands });
 });
 
 app.post('/generate-emails', upload.fields([{ name: 'serviceHeaderImage', maxCount: 1 }, { name: 'tireHeaderImage', maxCount: 1 }, { name: 'directory', maxCount: 100 }]), async (req, res) => {
-    const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    const jsonData = JSON.parse(await fs.readFile('data.json', 'utf8'));
     const archive = archiver('zip');
     const tmpDirs = [];
 
     for (let category of ['service', 'tire']) {
         for (let brand of Object.values(jsonData[category])) {
-            const brandObjectName = getBrandObjectNameFromName(jsonData, brand.name);
+            const brandObjectName = await getBrandObjectNameFromName(jsonData, brand.name);
             if (!brandObjectName) {
                 console.error(`No object name found for brand: ${brand.name}`);
                 continue;
             }
 
             const brandData = jsonData[category][brandObjectName];
-            const tmpDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), `${category}-${brandObjectName}-`));
+            const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), `${category}-${brandObjectName}-`));
             tmpDirs.push(tmpDir);
 
             const imagePath = path.join(tmpDir, 'images');
-            await fs.promises.mkdir(imagePath);
+            await fs.mkdir(imagePath);
 
-            await fs.promises.copyFile(brandData.logoURL, path.join(imagePath, 'logo.jpg'));
-            await fs.promises.copyFile(req.files[`${category}HeaderImage`][0].path, path.join(imagePath, 'header.jpg'));
+            await fs.copyFile(brandData.logoURL, path.join(imagePath, 'logo.jpg'));
+            await fs.copyFile(req.files[`${category}HeaderImage`][0].path, path.join(imagePath, 'header.jpg'));
+
+            if (brandData.footerGraphic) {
+                const footerGraphicFileName = path.basename(brandData.footerGraphic);
+                await fs.copyFile(brandData.footerGraphic, path.join(imagePath, footerGraphicFileName));
+                brandData.footerGraphic = path.join('images', footerGraphicFileName);
+            }
 
             const couponNumbers = req.body[`${brand.name}Coupons`].split(',').map(num => num.trim());
-            const couponFiles = req.files['directory'].filter(file => couponNumbers.includes(file.originalname.split('_')[0]));
-            
+            const couponFiles = couponNumbers.map(num => {
+                return req.files['directory'].find(file => file.originalname.startsWith(num + '_'));
+            });
+
             const couponDimensions = {};
             for (let file of couponFiles) {
-                await fs.promises.copyFile(file.path, path.join(imagePath, file.originalname));
+                await fs.copyFile(file.path, path.join(imagePath, file.originalname));
                 couponDimensions[file.originalname] = await getImageDimensions(path.join(imagePath, file.originalname));
             }
 
             const couponPaths = couponFiles.map(file => ({
                 path: path.join('images', file.originalname),
-                dimensions: couponDimensions[file.originalname],
-                alt: getAltTextFromFileName(file.originalname)
+                dimensions: couponDimensions[file.originalname]
             }));
 
             const emailContent = await ejs.renderFile('./views/email_template.ejs', {
@@ -116,12 +121,13 @@ app.post('/generate-emails', upload.fields([{ name: 'serviceHeaderImage', maxCou
                 headerLinks: brandData.headerLinks,
                 footerLinks: brandData.footerLinks,
                 phone: brandData.phone,
-					 notification: brandData.notification,
                 name: brandData.name,
-                coupons: couponPaths
+                coupons: couponPaths,
+                notification: brandData.notification,
+                footerGraphic: brandData.footerGraphic
             });
 
-            fs.writeFileSync(path.join(tmpDir, 'index.html'), emailContent);
+            await fs.writeFile(path.join(tmpDir, 'index.html'), emailContent);
             archive.directory(tmpDir, `${category}/${brandObjectName}`);
         }
     }
@@ -130,9 +136,9 @@ app.post('/generate-emails', upload.fields([{ name: 'serviceHeaderImage', maxCou
     res.attachment('emails.zip');
     archive.pipe(res);
 
-    res.on('finish', () => {
+    res.on('finish', async () => {
         for (let dir of tmpDirs) {
-            fs.promises.rm(dir, { recursive: true });
+            await fs.rm(dir, { recursive: true });
         }
     });
 });
